2e75669dc5053edadd09caa999e45e338ce22c88
Taddi Rajesh <92358291+rajeshtaddi@users.noreply.github.com>
2023-08-09 16:22:33 +0530

    Add files via upload

---
+++ VehicleLoanProject/src/dao/LoanDAO.java
@@ -0,0 +1,11 @@
+package dao;
+import java.util.List;
+import vehicle.*;
+public interface LoanDAO {
+        public void insertLoanApplication(Loan loan);
+        public Loan selectLoanApplication(int appliId);
+        public List<Loan> selectLoanApplications();
+        public void updateLoanApplication(Loan loan);
+        public void deleteLoanApplication(Loan loan);
+        
+}
